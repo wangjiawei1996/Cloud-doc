e7854eb1b6e14a5a69524cb13703a1fb7016f043
王嘉巍 <906789088@qq.com>
2019-10-21 20:25:51 +0800

    fileinput in search and btn in react css to feed this list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import FileSearch from './components/FileSearch'
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
         <div className="col-3 bg-danger left-panel">
-          <h1>this is the left</h1>
+          <FileSearch
+            title='我的云文档'
+            onFileSearch={() => {}}
+          />
         </div>
         <div className="col-9 bg-primary right-panel">
           <h1>this is the right</h1>
