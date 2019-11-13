9bc5c26da727f0dc42843bc70900e26afc647092
王嘉巍 <906789088@qq.com>
2019-11-13 20:16:53 +0800

    finish BottomBtn in FileList and set mb and pd in this

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import FileSearch from './components/FileSearch'
 import FileList from './components/FileList'
+import BottomBtn from './components/BottomBtn'
 import defaultFiles from './utils/defaultFiles'
 function App() {
   return (
-    <div className="App container-fluid">
-      <div className="row">
+    <div className="App container-fluid px-0">
+      <div className="row no-gutters">
         <div className="col bg-light left-panel">
           <FileSearch
             onFileSearch={(value) => {console.log(value)}}
@@ -18,6 +20,22 @@ function App() {
             onFileDelete={(id) => {console.log('deleting', id)}}
             onSaveEdit={(id, newValue) => {console.log(id); console.log(newValue)}}
           />
+          <div className='row no-gutters'>
+            <div className='col'>
+              <BottomBtn
+                text='新建'
+                colorClass='btn-primary'
+                icon={faPlus}
+              />
+            </div>
+            <div className='col'>
+              <BottomBtn
+                text='新建'
+                colorClass='btn-success'
+                icon={faFileImport}
+              />
+            </div>
+          </div>
         </div>
         <div className="col bg-primary right-panel">
           <h1>this is the right</h1>
---
+++ src/components/BottomBtn.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const BottomBtn = ({ text, colorClass, icon, onBtnClck}) => (
+  <button
+    type='button'
+    className={`btn btn-block no-border ${colorClass}`}
+    onClick={onBtnClck}
+  >
+    <FontAwesomeIcon
+      className="mr-2"
+      size="lg"
+      icon={icon}
+    />
+    {text}
+  </button>
+)
+BottomBtn.propTypes = {
+  text: PropTypes.string,
+  colorClass: PropTypes.string,
+  icon: PropTypes.element.isRequired,
+  onBtnClick: PropTypes.func
+}
+BottomBtn.defaultProps = {
+  text: '新建'
+}
+export default BottomBtn
