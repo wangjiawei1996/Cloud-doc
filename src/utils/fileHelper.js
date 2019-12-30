0237e6a07ba82efe9d3a0aad7d4c752fef5b96d5
王嘉巍 <906789088@qq.com>
2019-12-30 11:38:46 +0800

    add up readFIle, writeFIle renanmeFile deleteFIle in helper.js

--- src/utils/fileHelper.js
+++ src/utils/fileHelper.js
@@ -1,26 +1,31 @@
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 const fileHelper = {
-  readFile: (path, cb) => {
-    fs.readFile(path, { encoding: 'utf8'}, (err, data) => {
-      if(!err) {
-        cb(data)
-      }
-    })
+  readFile: (path) => {
+    return fs.readFile(path, { encoding: 'utf8'})
   },
-  writeFile: (path, content, cb) => {
-    fs.writeFile(path, content, { encoding: 'utf8'}, (err) => {
-      if (!err) {
-        cb()
-      }
-    })
+  writeFile: (path, content) => {
+    return fs.writeFile(path, content, { encoding: 'utf8'})
+  },
+  renameFile: (path, newPath) => {
+    return fs.rename(path, newPath)
+  },
+  deleteFile: (path) => {
+    return fs.unlink(path)
   }
 }
 const testPath = path.join(__dirname, 'helper.js')
 const testWritePath = path.join(__dirname, 'hello.md')
-fileHelper.readFile(testPath, (data) => {
-  console.log(data)
-})
-fileHelper.writeFile(testWritePath, '##hello world', () => {
-  console.log('写入成功')
+const renamePath = path.join(__dirname, 'rename.md')
+// fileHelper.readFile(testPath).then((data) => {
+//   console.log(data)
+// })
+// fileHelper.writeFile(testWritePath, '##hello world').then(() => {
+//   console.log('写入成功')
+// })
+// fileHelper.renameFile(testWritePath, renamePath).then(() => {
+//   console.log('重命名成功')
+// })
+fileHelper.deleteFile(renamePath).then(() => {
+  console.log(`${renamePath}删除成功`)
 })
