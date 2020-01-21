bd3e771ff5396539b1501f5acbacc960603f8b96
王嘉巍 <906789088@qq.com>
2020-01-21 10:58:01 +0800

    currentFIle isLoaded in readFile and use new path

--- src/App.js
+++ src/App.js
@@ -45,6 +45,13 @@ function App() {
   
   const fileClick = (fileID) => {
     setActiveFileID(fileID)
+    const currentFile = files[fileID]
+    if (!currentFile.isLoaded) {
+      fileHelper.readFile(currentFile.path).then(value => {
+        const newFile = { ...files[fileID], body: value, isLoaded: true }
+        setFiles({ ...files, [fileID]: newFile})
+      })
+    }
     if (!openedFileIDs.includes(fileID)) {
       setOpenedFileIDs([ ...openedFileIDs, fileID ])
     }
