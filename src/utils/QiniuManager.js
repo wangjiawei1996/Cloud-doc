dd234e011688eae420609765f35e14385a1536c0
王嘉巍 <906789088@qq.com>
2020-02-07 11:37:17 +0800

    get the download link, request it, return a readStream,create writable steam and pipe

--- src/utils/QiniuManager.js
+++ src/utils/QiniuManager.js
@@ -1,5 +1,6 @@
 const qiniu = require('qiniu')
-
+const axios = require('axios')
+const fs = require('fs')
 class QiniuManager {
   constructor( accessKey, secretKey, bucket ) {
     this.mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
@@ -44,6 +45,27 @@ class QiniuManager {
       } else {
         throw Error('域名未找到， 请查看存储空间是否已经过期')
       }
+    })
+  }
+  downloadFile(key, downloadPath) {
+    return this.generateDownloadLink(key).then(link => {
+      const timeStamp = new Date().getTime()
+      const url = `${link}?timestamp = ${timeStamp}`
+      return axios({
+        url,
+        method: 'GET',
+        responseType: 'stream',
+        headers: { 'Catch-Control': 'no-cache'}
+      })
+    }).then(response => {
+      const writer = fs.createWriteStream(downloadPath)
+      response.data.pipe(writer)
+      return new Promise((resolve, reject) => {
+        writer.on('finish', resolve)
+        writer.on('finish', reject)
+      })
+    }).catch(err => {
+      return Promise.reject({ err: err.response})
     })
   }
   _handleCallback(resolve, reject) {
