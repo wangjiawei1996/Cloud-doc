f96f1d3d00d93f68e743fa68eff0d7f7bae5eaeb
王嘉巍 <906789088@qq.com>
2020-02-08 12:02:50 +0800

    create windowBrowser appkey accesskey data set style block

--- settings/settings.js
+++ settings/settings.js
@@ -1,27 +1,39 @@
 const { remote } = require('electron')
 const Store = require('electron-store')
 const settingsStore = new Store({name: 'Settings'})
-const $ = (id) => {
-  return document.getElementById(id)
+const $ = (selector) => {
+  const result = document.querySelectorAll(selector)
+  return result.length > 1 ? result : result[0]
 }
 document.addEventListener('DOMContentLoaded', () => {
   let savedLocation = settingsStore.get('savedFileLocation')
   if (savedLocation) {
-    $('savedFileLocation').value = savedLocation
+    $('#savedFileLocation').value = savedLocation
   }
-  $('select-new-location').addEventListener('click', () => {
+  $('#select-new-location').addEventListener('click', () => {
     remote.dialog.showOpenDialog({
       properties: ['openDirectory'],
       message: '选择文件存储路径',
     }, (path) => {
       if (Array.isArray(path)) {
-        $('savedFileLocation').value = path[0]
+        $('#savedFileLocation').value = path[0]
         savedLocation = path[0]
       }
     })
   })
-  $('settings-form').addEventListener('submit', () => {
+  $('#settings-form').addEventListener('submit', () => {
     settingsStore.set('savedFileLocation', savedLocation)
     remote.getCurrentWindow().close()
+  })
+  $('.nav-tabs').addEventListener('click', (e) => {
+    e.preventDefault();
+    $('.nav-link').forEach(element => {
+      element.classList.remove('active')
+    })
+    e.target.classList.add('active')
+    $('.config-area').forEach(element => {
+      element.style.display = 'none'
+    })
+    $(e.target.dataset.tab).style.display = 'block'
   })
 })
